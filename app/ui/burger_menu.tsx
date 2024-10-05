ed6b505615d04088d05f693454c2bb8031797d69
Jorge Peris Navarro <jorge@Jorges-MacBook-Air.local>
2024-10-05 15:39:33 +0200

    fixed side menu

--- app/ui/burger_menu.tsx
+++ app/ui/burger_menu.tsx
@@ -1,26 +1,50 @@
 'use client';
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
+import classNames from 'classnames';
 
-export default function BurgerMenu({ position }: { position: string }) {
+export default function BurgerMenu({ position }: { position: 'left' | 'right' }) {
     const [showMenu, setShowMenu] = useState(false);
-    const menuRef = useRef<HTMLDivElement>(null);
+
+    const sideMenuCN = classNames(
+        'sm:w-1/6',
+        'w-1/3',
+        'fixed',
+        'top-0',
+        'h-screen',
+        'bg-white',
+        'ease-in-out',
+        'duration-300',
+        {
+            'sm:translate-x-0': showMenu,
+            'sm:translate-x-full': !showMenu,
+            'translate-x-0': showMenu,
+            '-translate-x-full': !showMenu,
+            'sm:right-0': position == 'right',
+            'left-0': position == 'left'
+        } 
+    )
+
+    const darkEffectCN = classNames(
+        'left-0',
+        'top-0',
+        'fixed',
+        'duration-300',
+        'ease-in-out',
+        'h-screen',
+        'w-screen',
+        'bg-black',
+        'transition-opacity',
+        'transition',
+        {
+            'opacity-60': showMenu,
+            'opacity-0': !showMenu
+        }
+    )
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
-        if (menuRef.current){
-            if (menuRef.current.classList.contains("hidden")){
-                menuRef.current.classList.remove("hidden");
-            }
-        }
     };
-
-    const handleTransitionEvent = () => {
-        if (!showMenu && menuRef.current) {
-            menuRef.current.classList.add("hidden");
-        }
-    };
-
     return (
         <div>
             <div onClick={toggleMenu}>
@@ -40,12 +64,13 @@ export default function BurgerMenu({ position }: { position: string }) {
                 </svg>
             </div>
 
-
-            <div onClick={toggleMenu} ref={menuRef} onTransitionEnd={handleTransitionEvent} className={`left-0 hidden top-0 fixed duration-300 ease-in-out h-screen w-screen bg-black transition-opacity transition ${showMenu ? 'opacity-60' : 'opacity-0'}`} />
             <div
-                className={`sm:w-1/6 w-1/3 fixed top-0 ${position}-0 h-screen bg-white ease-in-out duration-300 ${showMenu ? 'translate-x-0' : 'translate-x-full'
-                    }`}
-            >
+                onClick={showMenu ? toggleMenu : undefined}
+                className={darkEffectCN}
+                style={{ pointerEvents: showMenu ? 'auto' : 'none' }}
+            />
+            <div
+                className={sideMenuCN}>
                 <ul className="p-4">
                     <li>Home</li>
                     <li>About</li>
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -3,9 +3,9 @@ import BurgerMenu from './burger_menu';
 
 export default function Header() {
   return (
-    <div className="sm:pl-10 sm:pr-10 bg-[#f1f1f1] flex flex-row sm:justify-between items-center sm:pb-10 sm:pt-10 pt-5 pb-5">
+    <div className="sm:pl-10 sm:pr-10 bg-[#f1f1f1] flex sm:justify-between items-center sm:pb-10 sm:pt-10 pt-5 pb-5">
         <div className='sm:hidden ml-3'>
-        <BurgerMenu position='left' />
+          <BurgerMenu position='left' />
         </div>
       <div className='sm:gap-6 hidden sm:flex'>
         <SocialIcon url="https://x.com/" bgColor='#f1f1f1' fgColor='black' className='rounded-lg border-black border-2' />
